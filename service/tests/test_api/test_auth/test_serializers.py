ca810beb42902aab77d0bd21f5d0ddb1a6100b0d
Andrey Doroschenko <andrey.daraschenka@gmail.com>
2022-01-30 00:04:10 +0300

    Added test for SignUpSerializer

--- service/tests/test_api/test_auth/test_serializers.py
+++ service/tests/test_api/test_auth/test_serializers.py
@@ -1,9 +1,12 @@
+import factory
 import pytest
 from rest_framework import exceptions
 
 from api.v1.auth.serializers import SignUpSerializer
+from apps.user.models import User
 
 
+@pytest.mark.django_db
 class TestSignUpSerializer:
     @pytest.mark.parametrize('password', [
         'qwerty12',
@@ -12,3 +15,12 @@ class TestSignUpSerializer:
     def test_validate_password(self, password):
         with pytest.raises(exceptions.ValidationError):
             assert SignUpSerializer.validate_password(password)
+
+    def test_create_user_after_sign_up(self, user_factory):
+        serializer = SignUpSerializer(
+            data=factory.build(dict, FACTORY_CLASS=user_factory)
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        assert User.objects.exists()
