b025a52fdf34f860e60acce877d257ed59298ccc
Andrey Doroschenko <andrey.daraschenka@gmail.com>
2022-01-30 00:37:38 +0300

    Added tests for token auth status code

--- service/tests/factories/user.py
+++ service/tests/factories/user.py
@@ -14,6 +14,17 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        """Save original password for user in `_password` attribute."""
+
+        original_password = kwargs['password']
+
+        user = cls._get_manager(model_class).create_user(*args, **kwargs)
+        user._password = original_password
+
+        return user
+
 
 class MemberFactory(factory.django.DjangoModelFactory):
     user = factory.SubFactory(UserFactory)
--- service/tests/test_api/test_auth/test_views.py
+++ service/tests/test_api/test_auth/test_views.py
@@ -1,3 +1,4 @@
+import factory
 import pytest
 from rest_framework import status
 
@@ -5,12 +6,16 @@ from rest_framework import status
 @pytest.mark.django_db
 class TestAuthenticationViewSet:
     def test_sign_up_response_status(self, client, user_factory):
-        user = user_factory.build()
         assert client.post(
-            '/v1/auth/sign-up/', data={
-                'first_name': user.first_name,
-                'last_name': user.last_name,
+            '/v1/auth/token/sign-up/', data=factory.build(
+                dict, FACTORY_CLASS=user_factory
+            )
+        ).status_code == status.HTTP_201_CREATED
+
+    def test_sign_in_response_status(self, client, user):
+        assert client.post(
+            '/v1/auth/token/sign-in/', data={
                 'email': user.email,
-                'password': user.password
+                'password': user._password
             }
         ).status_code == status.HTTP_201_CREATED
