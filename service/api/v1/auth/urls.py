9e05cf0114097e1d33556031bae834d9b683914d
Andrey Doroschenko <andrey.daraschenka@gmail.com>
2022-05-31 17:07:40 +0300

    Validate auth token

--- service/api/v1/auth/urls.py
+++ service/api/v1/auth/urls.py
@@ -1,4 +1,6 @@
+from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenVerifyView
 
 from .google.views import GoogleAuthenticationViewSet
 
@@ -6,4 +8,6 @@ from .google.views import GoogleAuthenticationViewSet
 router = routers.SimpleRouter()
 router.register('google', GoogleAuthenticationViewSet, basename='google')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('verify/', TokenVerifyView.as_view(), name='token_verify')
+]
